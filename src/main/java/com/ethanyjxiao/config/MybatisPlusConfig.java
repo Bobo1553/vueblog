483c23af53ed085d3d143b18cb838ac4c7958c6b
ethan <646851868@qq.com>
2021-02-17 01:18:53 +0800

    first commit

---
+++ src/main/java/com/ethanyjxiao/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.ethanyjxiao.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Xiao Yijia
+ * @create 2021/2/17 1:05
+ */
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.ethanyjxiao.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
