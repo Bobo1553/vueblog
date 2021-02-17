6d27faff166603a5029336177a86446377ed0805
ethan <646851868@qq.com>
2021-02-18 01:53:59 +0800

    Shiro编码整合-登录逻辑开发

---
+++ src/main/java/com/ethanyjxiao/shiro/AccountProfile.java
@@ -0,0 +1,18 @@
+package com.ethanyjxiao.shiro;
+
+/**
+ * @author Xiao Yijia
+ * @create 2021/2/18 1:48
+ */
+
+public class AccountProfile {
+
+    private Long id;
+
+    private String username;
+
+    private String avatar;
+
+    private String email;
+
+}
--- src/main/java/com/ethanyjxiao/shiro/AccountRealm.java
+++ src/main/java/com/ethanyjxiao/shiro/AccountRealm.java
@@ -1,11 +1,14 @@
 package com.ethanyjxiao.shiro;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import com.ethanyjxiao.entity.User;
+import com.ethanyjxiao.service.UserService;
+import com.ethanyjxiao.util.JwtUtils;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,6 +18,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AccountRealm extends AuthorizingRealm {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserService userService;
 
     @Override
     public boolean supports(AuthenticationToken token) {
@@ -30,8 +39,18 @@ public class AccountRealm extends AuthorizingRealm {
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
         JwtToken jwtToken = (JwtToken) token;
 
-        System.out.println();
+        String userId = jwtUtils.getClaimByToken((String) jwtToken.getPrincipal()).getSubject();
 
-        return null;
+        User user = userService.getById(userId);
+        if(user == null){
+            throw new UnknownAccountException("账户不存在");
+        }else if (user.getStatus() == -1){
+            throw new LockedAccountException("账户已被锁定");
+        }
+
+        AccountProfile accountProfile = new AccountProfile();
+        BeanUtils.copyProperties(user, accountProfile);
+
+        return new SimpleAuthenticationInfo(accountProfile, jwtToken.getCredentials(), getName());
     }
 }
